51de3bb77d6789854eccf95adb5a8bb52f222518
Hing-Gu <wp05110@naver.com>
2019-12-27 14:14:04 +0900

    tab complete

--- app/src/main/java/com/example/tab/MainActivity.java
+++ app/src/main/java/com/example/tab/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,12 +20,43 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("MainActivity","here");
+                int pos = tab.getPosition();
+                Log.d("MainActivity",Integer.toString(pos));
+                changeView(pos);
             }
+
             public void onTabUnselected(TabLayout.Tab tab) {
             }
+
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
     }
+
+    private void changeView(int index) {
+        TextView testView1 = (TextView) findViewById(R.id.text1);
+        TextView testView2 = (TextView) findViewById(R.id.text2);
+        TextView testView3 = (TextView) findViewById(R.id.text3);
+
+        switch (index) {
+                case 0:
+                    testView1.setVisibility(View.VISIBLE);
+                    testView2.setVisibility(View.INVISIBLE);
+                    testView3.setVisibility(View.INVISIBLE);
+                    break;
+                case 1:
+                    testView1.setVisibility(View.INVISIBLE);
+                    testView2.setVisibility(View.VISIBLE);
+                    testView3.setVisibility(View.INVISIBLE);
+                    break;
+                case 2:
+                    testView1.setVisibility(View.INVISIBLE);
+                testView2.setVisibility(View.INVISIBLE);
+                testView3.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
 }
+
 
